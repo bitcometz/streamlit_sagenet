058264d272e13f85c429c94eaaf39705f806957c
bitcometz <novogene@163.com>
2024-04-26 14:12:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 import streamlit as st
+import scarches as sca
+import scanpy as sc # for plotting
+import anndata as ad # for handling the spatial and single-cell datasets
+import random # for setting a random seed
+import numpy as np
+import copy
+import squidpy as sq
+import pandas as pd
+from scarches.models.sagenet.utils import glasso
+from matplotlib import *
+import patchworklib as pw
+import functools
+
 st.write("hello")
+import torch_geometric
+st.write(torch_geometric.__version__)
