a0325b1bae2c1841b03cf5ad54ea4c07cee24218
bitcometz <novogene@163.com>
2024-04-26 15:14:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,9 @@
 import streamlit as st
-import sys
+import pkg_resources
 
-st.write("Python 版本号:", sys.version)
+# 获取已安装包的列表
+installed_packages = pkg_resources.working_set
 
-
-import scarches as sca
-import scanpy as sc # for plotting
-import anndata as ad # for handling the spatial and single-cell datasets
-import random # for setting a random seed
-import numpy as np
-import copy
-import squidpy as sq
-import pandas as pd
-from scarches.models.sagenet.utils import glasso
-from matplotlib import *
-import patchworklib as pw
-import functools
-
-st.write("hello")
-import torch_geometric
-st.write(torch_geometric.__version__)
+# 遍历每个已安装的包并输出其名称和版本号
+for package in installed_packages:
+    st.write(f"{package.key}=={package.version}")
