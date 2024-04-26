62181fb09a5146f1c2a7ed5f630513503b32911f
bitcometz <novogene@163.com>
2024-04-26 14:36:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
 import streamlit as st
-import scarches as sca
-import scanpy as sc # for plotting
-import anndata as ad # for handling the spatial and single-cell datasets
-import random # for setting a random seed
-import numpy as np
-import copy
-import squidpy as sq
-import pandas as pd
-from scarches.models.sagenet.utils import glasso
-from matplotlib import *
-import patchworklib as pw
-import functools
+import jax
+import jaxlib
+import jaxopt
+st.write(jax.__version__)
+st.write(jaxlib.__version__)
+st.write(jaxopt.__version__)
 
-st.write("hello")
-import torch_geometric
-st.write(torch_geometric.__version__)
