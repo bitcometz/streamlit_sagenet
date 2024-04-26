13a375fe141a13e22a1e6e0c238dd4dce1301256
bitcometz <novogene@163.com>
2024-04-26 14:38:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import jax
-import jaxlib
-import jaxopt
-st.write(jax.__version__)
-st.write(jaxlib.__version__)
-st.write(jaxopt.__version__)
+import pkg_resources
+
+# 获取已安装包的列表
+installed_packages = pkg_resources.working_set
+
+# 遍历每个已安装的包并输出其名称和版本号
+for package in installed_packages:
+    st.write(f"{package.key}=={package.version}")
 
